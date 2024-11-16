ec124ff798c59f188d7a6447bca8bdd4360e5e41
Peter Wang <peterwang135@gmail.com>
2024-11-16 01:16:23 -0500

    Created Navbar and Home Screen Screen with Introduction

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
+
+export default function Navbar() {
+  return (
+    <nav className="bg-white p-4 font-sans">
+        <div className = "flex justify-center">
+            <div className = "text-base flex space-x-12 font-medium text-gray-600">
+                <Link href="/" className="hover:border-b-2 hover:border-gray-300 px-1 py-2 transition-colors">Home</Link>
+                <Link href="/projects" className="hover:border-b-2 hover:border-gray-300 px-1 py-2 transition-colors">Projects</Link>
+                <Link href="/contact" className="hover:border-b-2 hover:border-gray-300 px-1 py-2 transition-colors">Contact</Link>
+            </div>
+        </div>
+
+        <div className = "right-4 top-4 absolute flex space-x-4">
+            <Link href="https://github.com/Peterw135" className="text-gray-400 hover:text-black transition-colors">
+                <FaGithub size={25}/>
+            </Link>
+            <Link href="https://linkedin.com/in/your-profile" className="text-gray-400 hover:text-black transition-colors">
+                <FaLinkedin size={25}/>
+            </Link>
+        </div>
+    </nav>
+  )
+} 
