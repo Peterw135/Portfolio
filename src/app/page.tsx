c136d0928850365605c372e03fe72bd4d9f3bf14
Peter Wang <peterwang135@gmail.com>
2024-11-16 01:23:46 -0500

    Hopefully fix deployment issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ export default function Home() {
     <main className="flex min-h-screen items-center px-12">
       <div className="max-w-3xl">
         <h1 className="text-5xl font-bold tracking-tight">
-          Computer Science Student, aspiring Software Engineer
+          Computer Science Student, Aspiring Software Engineer
         </h1>
         <p className="mt-6 text-xl text-gray-600 dark:text-gray-400">
           I am a Senior at Georgia Institute of Technology Studying Machine Learning and Learning Full Stack Development.
-          Based in Metro Atlanta, I'm Seeking Full-Time Software Engineering Opportunities.
+          Based in Metro Atlanta, I am Seeking Full-Time Software Engineering Opportunities.
         </p>
       </div>
     </main>
