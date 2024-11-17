57f800aab79668fcd99246dcfb599c7109d61594
Peter Wang <peterwang135@gmail.com>
2024-11-17 17:20:17 -0500

    Changed Style of Website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 export default function Home() {
   return (
-    <main className="flex h-[calc(100vh-80px)] items-center px-12 overflow-hidden">
+    <main className="flex h-[calc(100vh-80px)] items-center justify-center px-12 overflow-hidden">
       <div className="max-w-3xl">
         <h1 className="text-5xl font-bold tracking-tight">
-          Computer Science Student, Aspiring Software Engineer
+          Computer Science Student, <span className="text-blue-400	">Aspiring Software Engineer</span>
         </h1>
         <p className="mt-6 text-xl text-gray-600 dark:text-gray-400">
-          I am a Senior at Georgia Institute of Technology Studying Machine Learning and Learning Full Stack Development.
-          Based in Metro Atlanta, I am Seeking Full-Time Software Engineering Opportunities.
+          Hello! My name is Peter Wang. I am a Senior at Georgia Institute of Technology 
+          Studying Machine Learning and Learning Full Stack Development. Based in Metro Atlanta, 
+          I am Seeking Full-Time Software Engineering Opportunities.
         </p>
       </div>
     </main>
